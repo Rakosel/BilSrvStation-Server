f7b6cd7bcff4c479a6f71f0f506c8f420a89ce57
root <root@BilSrvStation.BilSrvWork>
2022-05-14 23:11:24 +0400

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,  начинающиеся с «» будут проигнорированы, а пустое сообщение  отменяет процесс коммита.
     Коммитер:  root <root@BilSrvStation.BilSrvWork>
    
     На ветке master
     Изменения, которые будут включены в коммит:
    	изменено:      copy.sh
    	новый файл:    docs/_build/.buildinfo
    	новый файл:    docs/_build/.doctrees/environment.pickle
    	новый файл:    docs/_build/.doctrees/index.doctree
    	новый файл:    docs/_build/_sources/index.rst.txt
    	новый файл:    docs/_build/_static/alabaster.css
    	новый файл:    docs/_build/_static/base-stemmer.js
    	новый файл:    docs/_build/_static/basic.css
    	новый файл:    docs/_build/_static/custom.css
    	новый файл:    docs/_build/_static/doctools.js
    	новый файл:    docs/_build/_static/documentation_options.js
    	новый файл:    docs/_build/_static/file.png
    	новый файл:    docs/_build/_static/jquery-3.5.1.js
    	новый файл:    docs/_build/_static/jquery.js
    	новый файл:    docs/_build/_static/language_data.js
    	новый файл:    docs/_build/_static/minus.png
    	новый файл:    docs/_build/_static/plus.png
    	новый файл:    docs/_build/_static/pygments.css
    	новый файл:    docs/_build/_static/russian-stemmer.js
    	новый файл:    docs/_build/_static/searchtools.js
    	новый файл:    docs/_build/_static/translations.js
    	новый файл:    docs/_build/_static/underscore-1.13.1.js
    	новый файл:    docs/_build/_static/underscore.js
    	новый файл:    docs/_build/genindex.html
    	новый файл:    docs/_build/index.html
    	новый файл:    docs/_build/objects.inv
    	новый файл:    docs/_build/search.html
    	новый файл:    docs/_build/searchindex.js
    	новый файл:    docs/index.md
    	изменено:      docs/requirements.txt
    	новый файл:    mkdocs.yml
    	удалено:       requirements.txt

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #/bin/bash -xv 
-set -x
+#set -x
 sudo chown admin_share:technics -Rf /opt/SAMBA_SHARE/DebianISO/
-cp -Rf /opt/SAMBA_SHARE/DebianISO/install_ext/ ~/BilSrvStation_Server_PC/DebianISO
-cp -R /opt/SAMBA_SHARE/DebianISO/preseed.cfg ~/BilSrvStation_Server_PC/DebianISO/
-cp -R /opt/SAMBA_SHARE/DebianISO/pc.sh ~/BilSrvStation_Server_PC/DebianISO/
+cp -Rf /opt/SAMBA_SHARE/DebianISO/install_ext/ /opt/SAMBA_SHARE/BilSrvStation_Server_PC/DebianISO
+cp -Rf /opt/SAMBA_SHARE/DebianISO/preseed.cfg /opt/SAMBA_SHARE/BilSrvStation_Server_PC/DebianISO/
+cp -Rf /opt/SAMBA_SHARE/DebianISO/pc.sh /opt/SAMBA_SHARE/BilSrvStation_Server_PC/DebianISO/

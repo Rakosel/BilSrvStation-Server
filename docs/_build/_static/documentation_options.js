505c338469fd2ce859b85671612bd6bda385345e
admin <admin@BilSrvStation.BilSrvWork>
2023-12-01 23:27:18 +0400

    Пожалуйста, введите сообщение коммита для ваших изменений. Строки,  начинающиеся с «» будут проигнорированы, а пустое сообщение  отменяет процесс коммита.
     Коммитер:  admin <admin@BilSrvStation.BilSrvWork>
    
     На ветке master
     Ваша ветка опережает «origin/master» на 5 коммитов.
       (используйте «git push», чтобы опубликовать ваши локальные коммиты)
    
     Изменения, которые будут включены в коммит:
    	изменено:      docs/_build/.buildinfo
    	изменено:      docs/_build/.doctrees/build_doc.doctree
    	изменено:      docs/_build/.doctrees/cut_discr_cmd_shell.doctree
    	изменено:      docs/_build/.doctrees/cut_discr_pc.doctree
    	изменено:      docs/_build/.doctrees/cut_discr_pii2.doctree
    	изменено:      docs/_build/.doctrees/cut_discr_pii2_pack.doctree
    	изменено:      docs/_build/.doctrees/cut_discr_preseed.doctree
    	изменено:      docs/_build/.doctrees/environment.pickle
    	изменено:      docs/_build/.doctrees/full_discr_pii2.doctree
    	изменено:      docs/_build/.doctrees/index.doctree
    	изменено:      docs/_build/.doctrees/note.doctree
    	изменено:      docs/_build/.doctrees/only_code_pii2.doctree
    	изменено:      docs/_build/.doctrees/sourcelink.doctree
    	изменено:      docs/_build/.doctrees/structure.doctree
    	изменено:      docs/_build/_static/basic.css
    	изменено:      docs/_build/_static/documentation_options.js
    	изменено:      docs/_build/_static/pygments.css
    	изменено:      docs/_build/build_doc.html
    	изменено:      docs/_build/cut_discr_cmd_shell.html
    	изменено:      docs/_build/cut_discr_pc.html
    	изменено:      docs/_build/cut_discr_pii2.html
    	изменено:      docs/_build/cut_discr_pii2_pack.html
    	изменено:      docs/_build/cut_discr_preseed.html
    	изменено:      docs/_build/full_discr_pii2.html
    	изменено:      docs/_build/genindex.html
    	изменено:      docs/_build/index.html
    	изменено:      docs/_build/note.html
    	изменено:      docs/_build/only_code_pii2.html
    	изменено:      docs/_build/search.html
    	изменено:      docs/_build/searchindex.js
    	изменено:      docs/_build/sourcelink.html
    	изменено:      docs/_build/structure.html

--- docs/_build/_static/documentation_options.js
+++ docs/_build/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.2a',
+    VERSION: '1.0.3a',
     LANGUAGE: 'ru,en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
